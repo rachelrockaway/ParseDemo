774e466a59972638a6b99aeb1933cdfd73df7bdf
Rachel Stevenson <rachel@rachelrockaway.com>
2015-03-31 15:49:44 -0700

    First commit

---
+++ ParseDemo/APIKeys.swift
@@ -0,0 +1,12 @@
+//
+//  APIKeys.swift
+//  ParseDemo
+//
+//  Created by Rachel Stevenson on 3/31/15.
+//  Copyright (c) 2015 rachelrockaway. All rights reserved.
+//
+
+import Foundation
+
+let kApplicationID = "HaMS9A6vxgINw4OKMWEW0aMqpEzKsCBeOBL4LudM"
+let kClientID = "SNzcrB5NybFndei84LyqaCqztu1tZe9Me6W52mUj"
